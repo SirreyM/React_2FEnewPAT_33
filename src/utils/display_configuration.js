96a13400b32e011220480a552fafe5ec9f5b9dc6
Administrator <Administrator@172.31.36.201>
2024-01-23 12:15:13 +0000

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,34 @@
+export const appointmentViewConfig = {
+Details: [
+    {
+      key: "Appointment_Id",
+      value: "Appointment_Id",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Reasonproblem",
+      value: "Reasonproblem",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "DateOfappointment",
+      value: "DateOfappointment",
+      type: "date",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+    {
+      key: "AppointmentVisit",
+      value: "AppointmentDetails",
+      type: "lookup",
+      editable: true,
+      required: true
+    },
+  ],
+}
